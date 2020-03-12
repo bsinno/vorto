452fd59d7fd4f2af7059e75326c6aa6a26e25748
OLK1BE <kevin.olotu@bosch.io>
2020-03-12 15:22:01 +0100

    Removed Mozilla lib.
    Signed-off-by: kolotu <kevin.olotu@bosch.io>

--- generators/generator-webofthings/src/main/java/org/eclipse/vorto/codegen/webofthings/WebOfThingsGenerator.java
+++ generators/generator-webofthings/src/main/java/org/eclipse/vorto/codegen/webofthings/WebOfThingsGenerator.java
@@ -11,85 +11,155 @@
  */
 package org.eclipse.vorto.codegen.webofthings;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Optional;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.eclipse.vorto.core.api.model.datatype.ConstraintRule;
+import org.eclipse.vorto.core.api.model.datatype.Property;
 import org.eclipse.vorto.core.api.model.functionblock.FunctionBlock;
+import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel;
 import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
 import org.eclipse.vorto.plugin.generator.GeneratorPluginInfo;
 import org.eclipse.vorto.plugin.generator.ICodeGenerator;
 import org.eclipse.vorto.plugin.generator.IGenerationResult;
 import org.eclipse.vorto.plugin.generator.InvocationContext;
+import org.eclipse.vorto.plugin.generator.adapter.ObjectMapperFactory;
 import org.eclipse.vorto.plugin.generator.utils.Generated;
 import org.eclipse.vorto.plugin.generator.utils.SingleGenerationResult;
-import org.json.JSONObject;
-import org.mozilla.iot.webthing.Action;
-import org.mozilla.iot.webthing.Event;
-import org.mozilla.iot.webthing.Property;
-import org.mozilla.iot.webthing.Thing;
-import org.mozilla.iot.webthing.Value;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class WebOfThingsGenerator implements ICodeGenerator {
 
-  private final String version;
+    private final String version;
 
-  public WebOfThingsGenerator() {
-    version = loadVersionFromResources();
-  }
+    public WebOfThingsGenerator() {
+        version = loadVersionFromResources();
+    }
 
-  @Override
-  public IGenerationResult generate(InformationModel model, InvocationContext context) {
-    Thing webThing = new Thing("urn:" +
-        model.getNamespace() + ":" + model.getName() + ":" + model.getVersion(),
-        model.getDisplayname());
+    @Override public IGenerationResult generate(InformationModel model, InvocationContext context) {
+        String result = null;
+        ObjectNode thingTemplate = newObjectNode();
+        thingTemplate.put("@context", "https://www.w3.org/2019/wot/td/v1");
+        thingTemplate.put("@type", "ThingTemplate");
+        thingTemplate.put("title", model.getName() + " Thing Description Template");
 
-    model.getProperties().forEach(fb -> {
-        Optional<FunctionBlock> extendedFunctionBlock = Optional.ofNullable(fb.getExtendedFunctionBlock());
-      Value<String> v = new Value("");
-      Property<String> p = new Property<>(webThing, fb.getType().getNamespace() + ":" + fb.getName() + ":" + fb.getType().getVersion(), v);
-      webThing.addProperty(p);
+        createPropertiesNode(model, thingTemplate);
+        addActionsNode(model, thingTemplate);
 
-      extendedFunctionBlock.ifPresent(efb -> {
-        Optional.ofNullable(efb.getEvents())
-            .ifPresent(events -> events
-                .stream()
-                .filter(Objects::nonNull)
-                .forEach(e -> webThing.addEvent(new Event<>(webThing, e.getName(), new ArrayList<>(e.getProperties())))));
-        Optional.ofNullable(efb.getOperations())
-            .ifPresent(operations -> operations
-                .stream()
-                .filter(Objects::nonNull)
-                .forEach(o -> {
-                  JSONObject metaData = new JSONObject();
-                  metaData.put("description", o.getDescription());
-                  metaData.put("label", o.getName());
-                  metaData.put("params", "TODO");
-                  webThing.addAvailableAction(o.getName(), metaData, Action.class);
+
+
+        ObjectNode events = newObjectNode();
+
+
+
+        thingTemplate.set("events", events);
+
+
+
+        return createResult(thingTemplate.toString());
+    }
+
+    private void createPropertiesNode(InformationModel model, ObjectNode thingTemplate) {
+        ObjectNode properties = newObjectNode();
+        model.getProperties().forEach(functionblockProperty -> {
+
+            String functionblockName = functionblockProperty.getName();
+            String functionblockDescription = functionblockProperty.getDescription();
+            if (Objects.nonNull(functionblockProperty.getPresence())) {
+                boolean isFunctionblockMandatory =
+                    functionblockProperty.getPresence().isMandatory();
+            }
+            boolean isFunctionblockMultiplicity = functionblockProperty.isMultiplicity();
+
+
+            Optional.ofNullable(functionblockProperty.getType())
+                .map(FunctionblockModel::getFunctionblock).map(FunctionBlock::getStatus).ifPresent(
+                status -> status.getProperties()
+                    .forEach(statusProperty -> addStatusNode(properties, statusProperty)));
+        });
+        thingTemplate.set("properties", properties);
+    }
+
+    private void addEventsNode(InformationModel model, ObjectNode thingTemplate) {
+        ObjectNode events = newObjectNode();
+        model.getProperties().forEach(functionblockProperty -> {
+            Optional.ofNullable(functionblockProperty.getType())
+                .map(FunctionblockModel::getFunctionblock).map(FunctionBlock::getEvents)
+                .ifPresent(fbEvents -> fbEvents.forEach(event -> {
+                    ObjectNode eventNode = newObjectNode();
+                    String eventName = event.getName();
+                    event.getProperties().forEach(eventProperty -> {
+                        String eventPropertyName = eventProperty.getName();
+                        String eventPropertyDescription = eventProperty.getDescription();
+                        eventProperty.getConstraintRule().getConstraints().forEach(
+                            eventConstraint -> putIfNotNull(eventNode,
+                                eventConstraint.getType().getName(),
+                                eventConstraint.getConstraintValues()));
+                    });
+                    events.set(eventName, eventNode);
                 }));
-        Optional.ofNullable(efb.getStatus()).ifPresent(status -> status.getProperties()
-            .stream()
-            .filter(Objects::nonNull)
-            .forEach(statusProperty -> {
-              // TODO
-        }));
-      });
-    });
+        });
+        thingTemplate.set("events", events);
+    }
 
-    Generated generated = new Generated("thingdefinition.json", "/",
-        webThing.asThingDescription().toString().getBytes());
-    SingleGenerationResult generationResult = new SingleGenerationResult("application/json");
-    generationResult.write(generated);
-    return generationResult;
-  }
+    private void addActionsNode(InformationModel model, ObjectNode thingTemplate) {
+        ObjectNode actions = newObjectNode();
+        model.getProperties().forEach(functionblockProperty -> {
+            Optional.ofNullable(functionblockProperty.getType())
+                .map(FunctionblockModel::getFunctionblock).map(FunctionBlock::getOperations)
+                .ifPresent(operations -> operations.forEach(operation -> {
+                    ObjectNode operationNode = newObjectNode();
+                    String operationName = operation.getName();
+                    String operationDescription = operation.getDescription();
+                    boolean operationBreakable = operation.isBreakable();
+                    boolean operationExtension = operation.isExtension();
 
-  @Override
-  public GeneratorPluginInfo getMeta() {
-    return GeneratorPluginInfo.Builder("webofthings")
-        .withDescription(
-            "Generates the Web of Things Thing Description")
-        .withName("Web of Things")
-        .withVendor("Eclipse Vorto Team")
-        .withPluginVersion(version)
-        .build();
-  }
+                    putIfNotNull(operationNode, "description", operationDescription);
+                    putIfNotNull(operationNode, "breakable", String.valueOf(operationBreakable));
+                    putIfNotNull(operationNode, "extension", String.valueOf(operationExtension));
+                    actions.set(operationName, operationNode);
+                }));
+        });
+        thingTemplate.set("actions", actions);
+    }
+
+    private void addStatusNode(ObjectNode properties, Property statusProperty) {
+        ObjectNode statusPropertyNode = newObjectNode();
+        String statusName = statusProperty.getName();
+        String statusDescription = statusProperty.getDescription();
+        boolean isStatusMandatory = statusProperty.getPresence().isMandatory();
+        ConstraintRule statusConstraintRule = statusProperty.getConstraintRule();
+        statusConstraintRule.getConstraints().forEach(
+            statusConstraint -> putIfNotNull(statusPropertyNode,
+                statusConstraint.getType().getName(), statusConstraint.getConstraintValues()));
+        statusProperty.getPropertyAttributes().forEach(statusPropertyAttribute -> {
+            String statusPropertyAtttributeString = statusPropertyAttribute.toString();
+        });
+        putIfNotNull(statusPropertyNode, "description", statusDescription);
+        putIfNotNull(statusPropertyNode, "mandatory", String.valueOf(isStatusMandatory));
+        properties.set(statusName, statusPropertyNode);
+    }
+
+    private IGenerationResult createResult(String result) {
+        Generated generated = new Generated("thingdefinition.json", "/", result);
+        SingleGenerationResult generationResult = new SingleGenerationResult("application/json");
+        generationResult.write(generated);
+        return generationResult;
+    }
+
+    @Override public GeneratorPluginInfo getMeta() {
+        return GeneratorPluginInfo.Builder("webofthings")
+            .withDescription("Generates the Web of Things Thing Description")
+            .withName("Web of Things").withVendor("Eclipse Vorto Team").withPluginVersion(version)
+            .build();
+    }
+
+    private void putIfNotNull(ObjectNode node, String name, String value) {
+        if (Objects.nonNull(value))
+            node.put(name, value);
+    }
+
+    private ObjectNode newObjectNode() {
+        return ObjectMapperFactory.getInstance().createObjectNode();
+    }
 }
