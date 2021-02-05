70df061372756ba68951e22a65849ddb1a62036f
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2021-02-05 12:53:25 +0100

    gh_export
    small bug fixes
    
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/admin/GHMigrationController.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/admin/GHMigrationController.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2020 Contributors to the Eclipse Foundation
- * <p>
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
- * <p>
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.vorto.repository.web.admin;
@@ -122,7 +122,7 @@ public class GHMigrationController {
         try {
             IOUtils.copy(new ByteArrayInputStream(zipContent), response.getOutputStream());
             response.flushBuffer();
-            LOGGER.info(String.format("Model packaging and download with search expression: [%s] -- success.", searchExpression));
+            LOGGER.info(String.format("Model packaging and download with search expression: [%s] -- success.", expression));
         } catch (IOException e) {
             LOGGER.warn(
                     String.format(
@@ -211,7 +211,7 @@ public class GHMigrationController {
                                                             FORMAT_IMAGE_UNDER_FOLDER,
                                                             folder,
                                                             "image",
-                                                            fileName.substring(fileName.lastIndexOf("."))
+                                                            fileName.substring(fileName.lastIndexOf(".") + 1)
                                                     )
                                             );
                                             zos.putNextEntry(imageEntry);
